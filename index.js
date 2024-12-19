1847dce14d0f03630e4128b3c3da649bc3358ce3
Jake3511 <131283521+Jake3511@users.noreply.github.com>
2024-12-18 22:19:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ function getRandomLetters() { // gets seven random letters
         const img = document.createElement("img"); // creates an img element
 
         if (randomKey == '_') { // for the blank key since file name can't be generalized
-            img.src = `/graphics_data/Scrabble_Tiles/Scrabble_Tile_blank.jpg`
+            img.src = `./graphics_data/Scrabble_Tiles/Scrabble_Tile_blank.jpg`
             img.alt = `Scrabble Tile _`
             img.style.width = "65px"; 
             img.style.height = "63px";
@@ -157,7 +157,7 @@ function getRandomLetters() { // gets seven random letters
             container.appendChild(img);
         }
         else {
-            img.src = `/graphics_data/Scrabble_Tiles/Scrabble_Tile_${randomKey}.jpg`
+            img.src = `./graphics_data/Scrabble_Tiles/Scrabble_Tile_${randomKey}.jpg`
             img.alt = `Scrabble Tile ${randomKey}`;
             img.style.width = "65px";
             img.style.height = "63px";
